5797b5af90a1a90e559d58a01d5b045364c74784
Jaydeep21 <jaydeepdharamsey21@gmail.com>
2018-09-27 21:06:04 +0530

    updating navbar

--- modules/navbar.php
+++ modules/navbar.php
@@ -14,26 +14,26 @@
       <a class="navbar-brand" href="#">Bhukkads</a>
     </div>
     <ul class="nav navbar-nav">
-      <li><a href="#">Home</a></li>
+      <li><a href="http://localhost/FoodBlogger/index.php">Home</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Cusine<span class="caret"></span></a>
       <ul class="dropdown-menu" style="background:black;">
-      	<li><a href="#" value="indian" name="navval" style="color:white;">Indian</a></li>
-        <li><a href="#" value="chinese" name="navval" style="color:white;">Chineese</a></li>
-        <li><a href="#" value="italian" name="navval" style="color:white;">Italian</a></li>
+      	<li><a href="http://localhost/FoodBlogger/assets/php/search.php?cusinav=indian" style="color:white;">Indian</a></li>
+        <li><a href="http://localhost/FoodBlogger/assets/php/search.php?cusinav=chinese" style="color:white;">Chineese</a></li>
+        <li><a href="http://localhost/FoodBlogger/assets/php/search.php?cusinav=italian" style="color:white;">Italian</a></li>
       </ul>
       </li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Course<span class="caret"></span></a>
       <ul class="dropdown-menu" style="background:black;">
-      	<li><a href="http://localhost/FoodBlogger/modules/search.php" style="color:white;" value="starter">Starter</a></li>
-        <li><a href="#" value="maincourse" name="navval" style="color:white;">Main Course</a></li>
-        <li><a href="#" value="desert" name="navval" style="color:white;">Desert</a></li>
-        <li><a href="#" value="snacks" name="navval" style="color:white;">Snacks</a></li>
+      	<li><a href="http://localhost/FoodBlogger/assets/php/search.php?navval=starter" style="color:white;">Starter</a></li>
+        <li><a href="http://localhost/FoodBlogger/assets/php/search.php?navval=maincourse" style="color:white;">Main Course</a></li>
+        <li><a href="http://localhost/FoodBlogger/assets/php/search.php?navval=desert" style="color:white;">Desert</a></li>
+        <li><a href="http://localhost/FoodBlogger/assets/php/search.php?navval=snacks" style="color:white;">Snacks</a></li>
       </ul>
       </li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-    	<li><a href="../FoodBlogger/modules/login.php">Login</a></li>
-        <li><a href="../FoodBlogger/modules/signup.php">SignUp</a></li>
+    	<li><a href="http://localhost/FoodBlogger/modules/login.php">Login</a></li>
+        <li><a href="http://localhost/FoodBlogger/modules/signup.php">SignUp</a></li>
     </ul>
     
     <form class="navbar-form navbar-right" action="http://localhost/FoodBlogger/assets/php/search.php" action="GET">
