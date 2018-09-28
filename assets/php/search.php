024efdb3012a338175d2ff3a356ceaea9f16375a
Jaydeep21 <jaydeepdharamsey21@gmail.com>
2018-09-28 23:40:35 +0530

    added iamge in searching place

--- assets/php/search.php
+++ assets/php/search.php
@@ -1,48 +1,100 @@
+<?php
+    session_start();
+    include('../../modules/navbar.php');
+?>
 <html>
 <head>
-    <title></title>
-    <style></style>
+    <title>Result</title>
+    <style>
+        a{
+            text-decoration: none;
+        }
+        body{
+            display: block;
+        }
+        img
+        {
+            position: absolute;
+            left:10px;
+            width:190px;
+            height: 130px;
+            float:left;
+            border:1px solid black;
+        }
+        .product{
+            position: relative;
+            left:220px;
+        }
+    </style>
+    
 </head>
 <body>
     
     
     <?php
-    session_start();
-    
-    $ser=$_GET['search'];
-    $_SESSION['search']=$ser;
-        
-    $dbpass="jaydeep";
-    $dbhost="localhost";
-    $dbname="foodblog";
-    $dbuser="root";
-        
-    
-    $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    include('connection.php');
+    $url="http://localhost/FoodBlogger/modules/video.php";
+    $urli="http://localhost/FoodBlogger/assets/img/";
     if(!$conn){
         die('Connection Error'.mysqli_connect_error());
     }
     
     if(isset($_GET['submit'])){
     
-    $url="http://localhost/FoodBlogger/modules/video.php";
+    $ser=$_GET['search'];
+    $_SESSION['search']=$ser;
+    
     $sear="select * from videos where dname LIKE '%$ser%'";
     $result=mysqli_query($conn,$sear);
     if(!mysqli_query($conn,$sear)){
         echo"no results found";
     }
     else{
-        echo"<h2>You are searching for:".$ser;
+        echo"<h2>You are searching for:".ucwords($ser);
+        echo"<hr>";
         while($row=mysqli_fetch_assoc($result)){
-            
+            echo"<a href=".$url."?p_id=" . $row['id'] . "><img src=".$urli.$row['image']."></a>";
+            echo"<div class='product'><a href=".$url."?p_id=" . $row['id'] . "><h1>".$row['dname']."</h1></a>
+            <p><bold>Description:</bold>&ensp;".$row['description']."</p>";
+            echo"Uploaded by: ".$_SESSION['fname']." ".$_SESSION['lname'];
+            echo"</div><hr>";
+        }
+    }
+    }
+    
+    
+    if(isset($_GET['cusinav'])){
+        $cusinev=$_GET['cusinav'];
+        $cusqry="select * from videos where cusine='$cusinev'";
+        $result=mysqli_query($conn,$cusqry);
+        if(!$result){
+            echo"<script>alert query error</script>";
+        }
+        else{
+            echo"<h1>".ucwords($cusinev)." Cusines</h1>";
+            echo"<hr>";
+            while($row=mysqli_fetch_assoc($result)){
             echo"<div class='product'><a href=".$url."?p_id=" . $row['id'] . "><h1>".$row['dname']."</h1></a>
             <p><bold>Description:</bold>&ensp;".$row['description']."</p>
             </div><hr>";
         }
+        }
     }
-    }
-    if(isset($_GET['navval'])){
-        $cuis="select * from videos where cusine=''";
+    if(isset($_REQUEST['navval'])){
+        $navval=$_REQUEST['navval'];
+        $navqry="select * from videos where course='$navval'";
+        $result=mysqli_query($conn,$navqry);
+        if(!$result){
+            echo"<script>alert query error</script>";
+        }
+        else{
+            while($row=mysqli_fetch_assoc($result)){
+            echo "<h1>".ucwords($navval)." C</h1>";
+            echo"<div class='product'><a href=".$url."?p_id=" . $row['id'] . "><h1>".$row['dname']."</h1></a>
+            <p><bold>Description:</bold>&ensp;".$row['description']."</p>
+            </div><hr>";
+        }
+        }
     }
     ?>
 </body>
