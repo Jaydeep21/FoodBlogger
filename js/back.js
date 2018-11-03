b434b988420fab5f64ab885543b32c30b06f24b2
Jaydeep Dharamsey <33755235+Jaydeep21@users.noreply.github.com>
2018-11-03 20:53:59 +0530

    Delete back.js

--- js/back.js
+++
@@ -1,5 +0,0 @@
-$('.fullBackground').fullClip({
-                    images:['../img/1.jpg','../img/2.jpg','../img/3.jpg','../img/4.jpg','../img/5.jpg','../img/6.jpg','../img/7.jpg','../img/8.jpg','../img/9.jpg','../img/10.jpg'],
-                    transitionTime:2000,
-                    wait:5000
-                });
